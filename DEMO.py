841e1f4507879dfa8c7e565033fd57f61cb2e146
KAUE <139394184+kauefs@users.noreply.github.com>
2024-04-03 12:54:29 -0300

    DEMO.py

--- DEMO.py
+++ DEMO.py
@@ -28,7 +28,7 @@ with st.sidebar.container():
      with cols[1]:st.markdown('''©2024™''')
      with cols[2]:st.empty()
 # MAIN
-st.markdown('''![DatAVisi0n](/img/DataVision3.png)''')
+st.markdown('''![ƊⱭȾɅViƧi🧿Ƞ](img/DataVision3.png)''')
 st.divider()
 with st.container():
      cols = st.columns(3)
