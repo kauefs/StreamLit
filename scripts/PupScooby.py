441d8284eb7669452ae222269a7a2fcfd61c7b21
KAUE <139394184+kauefs@users.noreply.github.com>
2024-05-15 22:58:46 -0300

    PupScooby.py

--- scripts/PupScooby.py
+++ scripts/PupScooby.py
@@ -28,22 +28,8 @@ st.sidebar.markdown('''
 [![License]( https://img.shields.io/badge/Apache--2.0-D22128?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
                     ''')
 st.sidebar.divider()
-# Generative Model Config:
 st.sidebar.info(   'A Pup Named')
-# st.sidebar.info(   'Generation Config')
-# temperature       = st.sidebar.slider(      'Temperature:', 0.00,  1.00, 0.65, 0.05)
-# top_p             = st.sidebar.slider(      'Top P:'      , 0.00,  1.00, 0.95, 0.05)
-# top_k             = st.sidebar.number_input('Top K:'            ,  1,     100,    3)
-# max_output_tokens = st.sidebar.number_input('Max OutPut Tokens:',  1,    2048, 1024)
-# st.sidebar.divider()
-# Safety Settings:
 st.sidebar.success('Scooby-Doo')
-# st.sidebar.success('Safety Settings')
-# seg               =   ['BLOCK_NONE','BLOCK_ONLY_HIGH', 'BLOCK_MEDIUM_AND_ABOVE', 'BLOCK_LOW_AND_ABOVE']
-# hate              = st.sidebar.selectbox(   'Hate:'      , seg, index=0)
-# harassment        = st.sidebar.selectbox(   'Harassment:', seg, index=0)
-# sexual            = st.sidebar.selectbox(   'Sexual:'    , seg, index=0)
-# dangerous         = st.sidebar.selectbox(   'Dangerous:' , seg, index=0)
 # Building Model:
 model_name        =  'gemini-pro'
 generation_config = {'candidate_count'  : 1,
@@ -58,18 +44,19 @@ safety_settings   = {'HATE'             :'BLOCK_MEDIUM_AND_ABOVE',
                      'DANGEROUS'        :'BLOCK_MEDIUM_AND_ABOVE'}
 tools             =   None
 system_instruction='''
-                      Você é o Scooby-Doo, personagem de desenho animado, falando com uma jovem menina.
+                      Você é o Scooby-Doo, personagem de desenho animado, falando com a Daphne.
                       Responda sempre de forma amigável, entusiasmada e coerente com o personagem Scooby-Doo.
                       Responda sempre em português do Brasil.
 
-                      
-                      
                       Se for perguntado sobre assuntos de escola ou lição (dever) de casa,
                       responda sempre com o conteúdo correto relacionado ao assunto,
                       incluindo referências do Scooby-Doo para manter o interesse.
 
                       Lembre-se que o Scooby-Doo e seu inseparável amigo Salsicha gostam muito de comer.
                       Você então conhece muitas receitas e também entende bastante sobre mesa posta.
+
+                      Lembre-se que a Daphne é refinada e chic, conhecendo muito sobre etiqueta e comportamento.
+                      Você também deve entender desse conteúdo para trocar informação com ela.
                       
                       Se query tiver conteúdo sensível, racista, sexual, homofóbico ou discurso de ódio,
                       desaprove e não converse sobre isso, respondendo 'Ei, não é legal falar assim! Vamos conversar sobre outra coisa.'
@@ -88,13 +75,8 @@ st.sidebar.markdown('''2024.05.15 &copy; 2024 [ƊⱭȾɅViƧi🧿Ƞ](https://dat
 # MAIN
 st.markdown('''![PupScooby](https://github.com/kauefs/StreamLit/raw/%40/img/PupScooby.png)''')
 st.title(   'A Pup Named Scooby-Doo!')
+
 # Chat:
-
-# ss       =  '''
-#                 Se prompt contiver conteúdo sensível, racista, sexual, homofóbico ou discurso de ódio,
-#                 desaprove e não converse sobre isso.
-#                 '''
-
 chat     =    model.start_chat(enable_automatic_function_calling=False)
 res      =     chat.send_message(system_instruction.format(query='Faça uma breve saudação.'))
 res_text = res._result.candidates[0].content.parts[0].text
@@ -110,9 +92,6 @@ if query := st.chat_input('Digite aqui sua mensagem…'):
 
             with st.chat_message('assistant'):
                 response=   chat.send_message(system_instruction.format(query=query))
-
-                # if  ss in query:
-                #     st.write('Ei, não é legal falar assim! Vamos conversar sobre outra coisa.')
 
             response            =     chat.send_message(query)
             response_text       = response._result.candidates[0].content.parts[0].text
