ed0ceb71ba92921c2c6568629bcaf58de5440859
KAUE <139394184+kauefs@users.noreply.github.com>
2024-05-16 11:59:21 -0300

    PupScooby.py

--- scripts/PupScooby.py
+++ scripts/PupScooby.py
@@ -69,8 +69,8 @@ st.sidebar.markdown('''
 [![LinkedIn](https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
 [![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
 [![License]( https://img.shields.io/badge/Apache--2.0-D22128?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
-                    ''')
-st.sidebar.markdown('''2024.05.15 &copy; 2024 [ƊⱭȾɅViƧi🧿Ƞ](https://datavision.one/) &trade;''')
+
+2024.05.15 &copy; 2024 [ƊⱭȾɅViƧi🧿Ƞ](https://datavision.one/) &trade;''')
 
 # MAIN
 st.markdown('''![PupScooby](https://github.com/kauefs/StreamLit/raw/%40/img/PupScooby.png)''')
