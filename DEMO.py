537cd2e885f6063762ad248d42e796a49878a78a
KAUE <139394184+kauefs@users.noreply.github.com>
2024-05-16 15:40:54 -0300

    DEMO.py

--- DEMO.py
+++ DEMO.py
@@ -13,13 +13,6 @@ def get_UN_data():
 # SIDE:
 st.sidebar.title('ƊⱭȾɅViƧi🧿Ƞ')
 st.sidebar.divider(            )
-st.sidebar.markdown('''
-[![GitHub](  https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](                                 https://github.com/kauefs/)
-[![Medium](  https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](                                 https://medium.com/@kauefs)
-[![LinkedIn](https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](                               https://www.linkedin.com/in/kauefs/)
-[![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
-[![License]( https://img.shields.io/github/license/kauefs/StreamLit?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
-                    ''')
 st.sidebar.markdown('''Data Science **|** Computer Vision **|** ML **|** AI **|** ☁️ **|** CyberSecurity **|**  👨🏻‍💻''')
 with    st.sidebar.container(      ):
     if  st.button('StreamLit Hello'):
@@ -30,7 +23,14 @@ st.sidebar.divider(                       )
 st.sidebar.header('Data Analysis')
 PlaceHolder = st.sidebar.empty(  )
 st.sidebar.markdown(sys.version  )
-st.sidebar.markdown('''2024.04.01 &copy; 2024 ƊⱭȾɅViƧi🧿Ƞ &trade;''')
+st.sidebar.markdown('''
+[![GitHub](  https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](                                 https://github.com/kauefs/)
+[![Medium](  https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](                                 https://medium.com/@kauefs)
+[![LinkedIn](https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](                               https://www.linkedin.com/in/kauefs/)
+[![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
+[![License]( https://img.shields.io/github/license/kauefs/StreamLit?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
+
+2024.04.01 &copy; 2024 ƊⱭȾɅViƧi🧿Ƞ &trade;''')
 # MAIN:
 st.markdown('''![ƊⱭȾɅViƧi🧿Ƞ](https://raw.githubusercontent.com/kauefs/StreamLit/%40/img/DataVision3.png)''')
 st.divider()
