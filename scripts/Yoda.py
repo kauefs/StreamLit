97ce49e54163ed971138a7cf7caa400d2eb929b1
KAUE <139394184+kauefs@users.noreply.github.com>
2024-05-23 13:07:53 -0300

    Yoda.py

--- scripts/Yoda.py
+++ scripts/Yoda.py
@@ -97,13 +97,20 @@ model             = genai.GenerativeModel(model_name        =     model_name,
                                           tools             =    tools )
 st.sidebar.divider()
 st.sidebar.markdown('''
-[![GitHub](  https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](https://github.com/kauefs/)
-[![Medium](  https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](https://medium.com/@kauefs)
-[![LinkedIn](https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
-[![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
-[![License]( https://img.shields.io/badge/Apache--2.0-D22128?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
 
-2024.05.15 &copy; 2024 [ƊⱭȾɅViƧi🧿Ƞ](https://datavision.one/) &trade;''')
+![2024.05.15](  https://img.shields.io/badge/24.05.15-000000)
+![2024.05.15](  https://img.shields.io/badge/-000000?label=2024.05.15&labelColor=000000)
+![2024.05.15](  https://img.shields.io/badge/-000000?style=flat&logo=&logoColor=000000&label=2024.05.15&labelColor=000000&color=000000)
+
+[![GitHub](     https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](https://github.com/kauefs/)
+[![Medium](     https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](https://medium.com/@kauefs)
+[![LinkedIn](   https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
+[![Python](     https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
+
+[![License](    https://img.shields.io/badge/Apache--2.0-D22128?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
+
+[![ƊⱭȾɅViƧi🧿Ƞ](https://img.shields.io/badge/ƊⱭȾɅViƧi🧿Ƞ&trade;-0065FF?style=plastic&logo=&logoColor=0065FF&label=&copy;2024&labelColor=0065FF&color=0065FF)](https://datavision.one/)
+                    ''')
 
 # MAIN
 st.markdown('![Yoda](https://static.wikia.nocookie.net/starwars/images/c/c3/Yoda_TPM_RotS.png)')
