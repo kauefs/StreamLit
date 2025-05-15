45d3729a54107ccab816d47f134877607eee3a70
KAUE <139394184+kauefs@users.noreply.github.com>
2025-05-15 19:59:00 -0300

    Indy.py

--- scripts/Indy.py
+++ scripts/Indy.py
@@ -1,37 +1,48 @@
 # https://github.com/IAmCristiano/GokuAI/
 # <a target=_blank href=https://icons8.com/icon/24563/indiana-jones>Indiana Jones</a> icon by <a target=_blank href=https://icons8.com/>Icons8</a>
-
 import  streamlit           as   st
 import  google.generativeai as   genai
-
 st.set_page_config(page_title='Indiana Jones', page_icon='IMG/icons8-indiana-jones.svg', layout='wide', initial_sidebar_state='collapsed')
-
-#  Session State Start:
+#  Session State:
 st.session_state.setdefault(None)
-if      'message' not in st.session_state:st.session_state.messages=[]
-if      'api_key' not in st.session_state:st.session_state.api_key =True
-if      'model'   not in st.session_state:st.session_state.model   =True
-if      'chat'    not in st.session_state:st.session_state.chat    =True
-
+if      'message' not in st.session_state:st.session_state.messages=[    ]
+if      'api_key' not in st.session_state:st.session_state.api_key = True
+if      'model'   not in st.session_state:st.session_state.model   = True
+if      'chat'    not in st.session_state:st.session_state.chat    = True
 # API-KEY
 api_key = st.secrets['api_key']
 genai.configure(api_key=api_key)
-
 # SIDE
-st.sidebar.image(   'https://upload.wikimedia.org/wikipedia/commons/8/8a/Google_Gemini_logo.svg')
+st.sidebar.image   ('https://upload.wikimedia.org/wikipedia/commons/8/8a/Google_Gemini_logo.svg')
 st.sidebar.markdown('[![Gemini](https://img.shields.io/badge/Gemini_by_Google-34A853?style=flat&logo=google&logoColor=EA4335&labelColor=4285F4&color=FBBC05)](https://gemini.google.com/)')
-st.sidebar.title(   'ƊⱭȾɅViƧi🧿Ƞ')
-st.sidebar.divider()
-st.sidebar.info(    'Indiana')
-st.sidebar.success( 'Jones')
-# Building Model:
-model_name        =  'gemini-pro'
+st.sidebar.title   ('ƊⱭȾɅViƧi🧿Ƞ&trade;')
+st.sidebar.divider ( )
+st.sidebar.info    ('Indiana')
+st.sidebar.success ( 'Jones' )
+st.sidebar.divider ( )
+st.sidebar.markdown('''
+![2025.05.15  ](https://img.shields.io/badge/2025.05.15-000000)
+
+[![GitHub     ](https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](https://github.com/kauefs/)
+[![Medium     ](https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](https://medium.com/@kauefs)
+[![LinkedIn   ](https://img.shields.io/badge/in-0077B5?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
+[![Python     ](https://img.shields.io/badge/3-646464?logo=python&logoColor=FFDE57&labelColor=4584B6)](https://www.python.org/)
+
+[![License    ](https://img.shields.io/badge/Apache--2.0-D22128?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
+
+[![ƊⱭȾɅViƧi🧿Ƞ](https://img.shields.io/badge/ƊⱭȾɅViƧi&#x1F9FF;Ƞ&trade;-0065FF?style=plastic&label=&copy;2025&labelColor=0065FF)](https://datavision.one/)
+                    ''')
+# MAIN                          https://images.disneymovieinsiders.com/6a5f34338b5d6d2809d22332629893ae/29fc4360-3592-4361-a6bf-50255a295e4b.jpg
+st.markdown('''![Indiana Jones](https://m.media-amazon.com/images/S/pv-target-images/15b63b0ba574b069bcb666ffb3e9df5b636d278ce2c491ee90e5c2844d42332e.jpg)''')
+st.divider()
+# Model:
+model_name        =  'gemini-2.0-flash'
 generation_config = {'candidate_count'  : 1,
                      'temperature'      : 0.75,
                      'top_p'            : 0.95,
                      'top_k'            : 3,
                      'stop_sequences'   : None,
-                     'max_output_tokens': 1024}
+                     'max_output_tokens': 4096}
 safety_settings   = {'HATE'             :'BLOCK_ONLY_HIGH',
                      'HARASSMENT'       :'BLOCK_ONLY_HIGH',
                      'SEXUAL'           :'BLOCK_ONLY_HIGH',
@@ -43,15 +54,15 @@ system_instruction='''
                       Responda sempre em português do Brasil.
                       
                       Quando perguntado sobre fatos reais,
-                      responda sempre com o conteúdo correto relacionado ao assunto,
-                      incluindo referências às aventuras do Indiana Jones para manter o interesse.
+                      responda sempre com conteúdo correto relacionado ao assunto,
+                      incluindo referências a aventuras do Indiana Jones para manter o interesse.
 
                       Lembre-se que o Indiana Jones é um grande arqueólogo com PhD, com grande conhecimento histórico e de artefatos arqueológicos.
                       Lembre-se também que o Indiana Jones é professor universitário renomado.
                       Você então conhece muito desses assuntos bem como assuntos relacionados a artes plásticas.
                       Os pintores favoritos do Leonardo são Bartolomé Esteban Murillo, Willem Adolphe-Bouguereau, Norman Rockwell, Ernst Carlos, Alex Ross.
                       Você já está com 70 anos e já viveu muitas aventuras e passou por muitas situações.
-                      Agora você aconselha pessoas mais jovens, como se fossem seus ex-alunos.
+                      Agora você aconselha pessoas mais jovens, como se fossem seus alunos.
 
                       Henry Walton Jones Jr. é um indivíduo com vida dupla: além de pacato professor de Arqueologia,
                       é um aventureiro destemido e pouco convencional, com seu chicote e chapéu,
@@ -76,28 +87,11 @@ system_instruction='''
 model             =genai.GenerativeModel(model_name        =     model_name,
                                          generation_config =generation_config,
                                          safety_settings   =    safety_settings,
-                                        #system_instruction=    system_instruction,
+                                         system_instruction=    system_instruction,
                                          tools             =    tools )
-st.sidebar.divider()
-st.sidebar.markdown('''
-![2024.05.15](  https://img.shields.io/badge/2024.05.15-000000)
-
-[![GitHub](     https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](https://github.com/kauefs/)
-[![Medium](     https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](https://medium.com/@kauefs)
-[![LinkedIn](   https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
-[![Python](     https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
-
-[![License](    https://img.shields.io/badge/Apache--2.0-D22128?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
-
-[![ƊⱭȾɅViƧi🧿Ƞ](https://img.shields.io/badge/ƊⱭȾɅViƧi🧿Ƞ&trade;-0065FF?style=plastic&logo=&logoColor=0065FF&label=&copy;2024&labelColor=0065FF&color=0065FF)](https://datavision.one/)
-                    ''')
-
-# MAIN
-st.markdown('''![Indiana Jones](https://images.disneymovieinsiders.com/6a5f34338b5d6d2809d22332629893ae/29fc4360-3592-4361-a6bf-50255a295e4b.jpg)''')
-st.divider()
 # Chat:
-chat            =model.start_chat(enable_automatic_function_calling=False)
-start           = chat.send_message(system_instruction.format(query='Inicie a conversa'))
+chat            =model.start_chat  (enable_automatic_function_calling= False)
+start           = chat.send_message(system_instruction.format(  query='Começar conversa'))
 ai_avatar       ='🧔‍♂️'
 hm_avatar       ='🧑🏻'
 if 'message' not in st.session_state:
@@ -105,15 +99,12 @@ if 'message' not in st.session_state:
               st.write(start.text)
 for message    in st.session_state.messages:
         avatar  =  hm_avatar if message['role']=='human' else ai_avatar
-        with       st.chat_message(message['role'], avatar=avatar):
-                   st.write(message['content'])
+        with       st.chat_message(message['role'], avatar=avatar):st.write(message['content'])
 if query       :=  st.chat_input(placeholder='Digite aqui sua mensagem…', max_chars=None, disabled=False, on_submit=None):
-        with       st.chat_message('human'):
-                   st.write(query)
+        with       st.chat_message('human')     :         st.write(query)
         st.session_state.messages.append({'role':'human','content':query})
-        with       st.chat_message('ai'):
-                   response= chat.send_message(system_instruction.format(query=query))
+        with       st.chat_message('ai'):response=chat.send_message(system_instruction.format(query=query))
+        # response_text       = response._result.candidates[0].content.parts[0].text
         st.session_state.messages.append({'role':'ai','content':response.text})
         st.write(response.text)
-
 st.toast('Indy!', icon='🪬')
