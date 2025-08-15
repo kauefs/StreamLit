2d7b6beb339ba6cab0ab478d7729835672498ad0
KAUE <139394184+kauefs@users.noreply.github.com>
2025-08-15 19:52:05 -0300

    trees.py

--- scripts/trees.py
+++ scripts/trees.py
@@ -19,6 +19,8 @@ FontY={'family':'sans-serif'    ,'color':'#FF4500', 'size': 10,    'fontweight':
 FontX={'family':'sans-serif'    ,'color':'#4CAF50', 'size': 10,    'fontweight':'regular'   }
 # Page:
 st.set_page_config(page_title='ƊⱭȾɅ Ʌnalysis', page_icon='📊', layout='wide', initial_sidebar_state='collapsed')
+def state( ):
+    if 'points' not in st.session_state:st.session_state.points=[]
 # SIDE
 st.sidebar.title   ('ƊⱭȾɅViƧi🧿Ƞ&trade;')
 st.sidebar.divider ( )
@@ -40,6 +42,7 @@ st.sidebar.markdown('''
 [![ƊⱭȾɅViƧi🧿Ƞ](https://img.shields.io/badge/ƊⱭȾɅViƧi🧿Ƞ&trade;-0065FF?style=plastic&label=&copy;2025&labelColor=0065FF)](https://datavision.one/)
                     ''')
 # MAIN:
+state( )
 st.title('San Francisco Trees')
 st.divider( )
 trees=pd.read_csv('https://github.com/PacktPublishing/Streamlit-for-Data-Science/raw/refs/heads/main/trees_app/trees.csv')
